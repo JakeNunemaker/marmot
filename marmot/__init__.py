e6f8f9c5fe40bc1b4e8b2746dcffc881c4dd593b
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2019-11-16 09:42:55 -0700

    Added initial module structure.

--- marmot/__init__.py
+++ marmot/__init__.py
@@ -6,3 +6,7 @@ Agent based process modeling using SimPy.
 """
 
 __version__ = "0.1.0"
+
+from .agent import Agent
+from .object import Object
+from .environment import Environment
---
+++ marmot/agent.py
@@ -0,0 +1,13 @@
+"""Agent base class for marmot process modeling."""
+
+__author__ = "Jake Nunemaker"
+__copyright__ = "Copyright 2019, Jake Nunemaker"
+__credits__ = ""
+# __version__ = "0.0.1"
+__maintainer__ = "Jake Nunemaker"
+__email__ = "jake.d.nunemaker@gmail.com"
+__status__ = "Development"
+
+
+class Agent:
+    pass
