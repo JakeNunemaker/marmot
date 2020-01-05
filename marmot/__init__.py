9a327368ff9dd0e6d72feb781c7ac4feba0733a1
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-01-05 11:47:11 -0700

    Modified setup.py to include link to simpy-agents. Added documentation setup and structure.

--- marmot/__init__.py
+++ marmot/__init__.py
@@ -2,12 +2,20 @@
 
 __author__ = "Jake Nunemaker"
 __copyright__ = "Copyright 2019, Jake Nunemaker"
-__version__ = "0.1.0"
 __maintainer__ = "Jake Nunemaker"
 __email__ = "jake.d.nunemaker@gmail.com"
 __status__ = "Development"
 
 
+import os
+
 from .agent import Agent, process
 from .object import Object
 from .environment import Environment
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.abspath(os.path.join(ROOT, "../VERSION"))) as version_file:
+    VERSION = version_file.read().strip()
+
+
+__version__ = VERSION
--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+"""Distribution setup."""
+
+import os
+
 from setuptools import setup
 
-import marmot
+ROOT = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+with open(os.path.join(ROOT, "VERSION")) as version_file:
+    __version__ = version_file.read().strip()
+
 
 setup(
     name="Marmot",
-    version=marmot.__version__,
+    version=__version__,
     description=long_description,
     classifiers=[
         "Development Status :: 1 - Planning",
@@ -16,7 +23,7 @@ setup(
     ],
     packages=["marmot"],
     install_requires=[
-        "simpy",
+        "simpy-agents @ http://github.com/JakeNunemaker/simpy-agents/tarball/master#egg=v0.1.0",
         "pyyaml",
         "pre-commit",
         "black",
